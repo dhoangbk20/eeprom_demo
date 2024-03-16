a6bfe99c2b1a47429faa8acca1069a7568b31820
LE DINH HOANG <hoang.leddt2079@hcmut.edu.vn>
2024-03-16 07:45:46 +0700

    Store DHT11 data sensor in EEPROM I2C

---
+++ dong_co/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace BTL_DL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.txt_humidflash = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.txt_tempeflash = new System.Windows.Forms.TextBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cBoxCom = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.txt_receivetemp = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txt_receivehumid = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox6);
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Controls.Add(this.btn_run);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.groupBox1.Location = new System.Drawing.Point(19, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 311);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DISPLAY";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.txt_humidflash);
+            this.groupBox6.Location = new System.Drawing.Point(0, 204);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(279, 101);
+            this.groupBox6.TabIndex = 9;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Humidity in EEPROM";
+            // 
+            // txt_humidflash
+            // 
+            this.txt_humidflash.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_humidflash.Location = new System.Drawing.Point(84, 14);
+            this.txt_humidflash.Multiline = true;
+            this.txt_humidflash.Name = "txt_humidflash";
+            this.txt_humidflash.Size = new System.Drawing.Size(124, 81);
+            this.txt_humidflash.TabIndex = 3;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox5.Controls.Add(this.txt_tempeflash);
+            this.groupBox5.Location = new System.Drawing.Point(6, 77);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(280, 121);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Temperature in EEPROM";
+            // 
+            // txt_tempeflash
+            // 
+            this.txt_tempeflash.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tempeflash.Location = new System.Drawing.Point(81, 19);
+            this.txt_tempeflash.Multiline = true;
+            this.txt_tempeflash.Name = "txt_tempeflash";
+            this.txt_tempeflash.Size = new System.Drawing.Size(108, 96);
+            this.txt_tempeflash.TabIndex = 3;
+            this.txt_tempeflash.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_run
+            // 
+            this.btn_run.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_run.Location = new System.Drawing.Point(100, 21);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(92, 37);
+            this.btn_run.TabIndex = 2;
+            this.btn_run.Tag = "";
+            this.btn_run.Text = "DISPLAY";
+            this.btn_run.UseVisualStyleBackColor = false;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnClose);
+            this.groupBox3.Controls.Add(this.btnOpen);
+            this.groupBox3.Controls.Add(this.cBoxBaudRate);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.cBoxCom);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(26, 336);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(286, 225);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Connect";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(150, 139);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(82, 37);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(35, 139);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(82, 37);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cBoxBaudRate
+            // 
+            this.cBoxBaudRate.FormattingEnabled = true;
+            this.cBoxBaudRate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "115200"});
+            this.cBoxBaudRate.Location = new System.Drawing.Point(80, 88);
+            this.cBoxBaudRate.Name = "cBoxBaudRate";
+            this.cBoxBaudRate.Size = new System.Drawing.Size(121, 24);
+            this.cBoxBaudRate.TabIndex = 3;
+            this.cBoxBaudRate.Text = "115200";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "BaudRate";
+            // 
+            // cBoxCom
+            // 
+            this.cBoxCom.FormattingEnabled = true;
+            this.cBoxCom.Location = new System.Drawing.Point(80, 35);
+            this.cBoxCom.Name = "cBoxCom";
+            this.cBoxCom.Size = new System.Drawing.Size(121, 24);
+            this.cBoxCom.TabIndex = 1;
+            this.cBoxCom.Text = "COM6";
+            this.cBoxCom.SelectedIndexChanged += new System.EventHandler(this.cBoxCom_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Com";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(318, 19);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(748, 384);
+            this.zedGraphControl1.TabIndex = 6;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // txt_receivetemp
+            // 
+            this.txt_receivetemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_receivetemp.Location = new System.Drawing.Point(19, 21);
+            this.txt_receivetemp.Multiline = true;
+            this.txt_receivetemp.Name = "txt_receivetemp";
+            this.txt_receivetemp.Size = new System.Drawing.Size(320, 106);
+            this.txt_receivetemp.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_receivetemp);
+            this.groupBox2.Location = new System.Drawing.Point(318, 415);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(354, 146);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Current Temperature Data";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txt_receivehumid);
+            this.groupBox4.Location = new System.Drawing.Point(712, 415);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(354, 146);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Current Humidity Data";
+            // 
+            // txt_receivehumid
+            // 
+            this.txt_receivehumid.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_receivehumid.Location = new System.Drawing.Point(19, 21);
+            this.txt_receivehumid.Multiline = true;
+            this.txt_receivehumid.Name = "txt_receivehumid";
+            this.txt_receivehumid.Size = new System.Drawing.Size(320, 106);
+            this.txt_receivehumid.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 573);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Quan sát dữ liệu cảm biến DHT11";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cBoxBaudRate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cBoxCom;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.TextBox txt_receivetemp;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txt_receivehumid;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox txt_humidflash;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox txt_tempeflash;
+    }
+}
+
---
+++ dong_co/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+using ZedGraph;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BTL_DL
+{
+    public partial class Form1 : Form
+    {
+        string Alldata;
+        string tempe,humid;
+        string tempe_flash,humid_flash;
+        string data;
+        int len;
+        int time;
+        double set;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            txt_receivetemp.ScrollBars = ScrollBars.Vertical;
+           
+            txt_receivehumid.ScrollBars = ScrollBars.Vertical;
+            txt_tempeflash.ScrollBars = ScrollBars.Vertical;
+
+            txt_humidflash.ScrollBars = ScrollBars.Vertical;
+            string[] ports = SerialPort.GetPortNames();
+            cBoxCom.Items.AddRange(ports);
+
+            GraphPane MyPanne = zedGraphControl1.GraphPane;
+            MyPanne.Title.Text = "Dữ liệu cảm biến";
+            MyPanne.YAxis.Title.Text = "Giá trị cảm biến";
+            MyPanne.XAxis.Title.Text = "Thời gian";
+
+            RollingPointPairList list1 = new RollingPointPairList(500000);
+            RollingPointPairList list2 = new RollingPointPairList(500000);
+
+            LineItem line1 = MyPanne.AddCurve("Temperature", list1, Color.Red, SymbolType.None);
+            LineItem line2 = MyPanne.AddCurve("Humidity", list2, Color.Blue, SymbolType.None);
+
+            MyPanne.XAxis.Scale.Min = 0;
+            MyPanne.XAxis.Scale.Max = 100;
+            MyPanne.XAxis.Scale.MinorStep = 1;
+            MyPanne.XAxis.Scale.MajorStep = 5;
+
+            MyPanne.YAxis.Scale.Min = 0;
+            MyPanne.YAxis.Scale.Max = 100;
+            MyPanne.YAxis.Scale.MinorStep = 2;
+            MyPanne.YAxis.Scale.MajorStep = 10;
+
+            zedGraphControl1.AxisChange();
+
+        }
+        double tong1 = 0;
+        double tong2 = 0;
+        public void draw1(double line1)
+        {
+            LineItem duongline1 = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (duongline1 == null )
+            {
+                return;
+            }
+            IPointListEdit list1 = duongline1.Points as IPointListEdit;
+            if (list1 == null )
+            {
+                return;
+            }
+            list1.Add(tong2, line1);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            tong1 += 0.5;
+
+        }
+        public void draw2(double line2)
+        {
+            LineItem duongline2 = zedGraphControl1.GraphPane.CurveList[1] as LineItem;
+            if ( duongline2 == null)
+            {
+                return;
+            }
+            IPointListEdit list2 = duongline2.Points as IPointListEdit;
+            if ( list2 == null)
+            {
+                return;
+            }
+            list2.Add(tong2, line2);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            tong2 += 0.5;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Start();
+                serialPort1.PortName = cBoxCom.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cBoxBaudRate.Text);
+                serialPort1.Open();
+            }
+            catch (Exception err) 
+            { 
+                MessageBox.Show(err.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }    
+        }
+        int count = 0;
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.IsOpen) 
+            {
+             Alldata = "";
+          
+             Alldata = serialPort1.ReadLine();
+                //Data receive if current data, frame data is xx/xx/r/n(6), if flash data, frame data is 1/xx/xx/r/n(8)
+             len = Alldata.Length;
+            if(len == 6)
+             {
+                    string[] data = Alldata.Split('/');
+                    DateTime currentTime = DateTime.Now;
+                    tempe = data[0];
+                    humid = data[1];
+
+                    Invoke(new MethodInvoker(() => draw1(Convert.ToDouble(tempe))));
+                    Invoke(new MethodInvoker(() => draw2(Convert.ToDouble(humid))));
+                    string tempe_1 = currentTime.ToString("HH:mm:ss:  ") + tempe + '\u2103' + "\r\n";
+                    string humid_1 = currentTime.ToString("HH:mm:ss:  ") + humid + "%RH" + "\r\n";
+                    txt_receivetemp.Invoke(new Action(() => txt_receivetemp.AppendText(tempe_1)));
+                    txt_receivehumid.Invoke(new Action(() => txt_receivehumid.AppendText(humid_1)));
+
+             }
+            else if(len == 8)
+            {
+                    string[] data = Alldata.Split('/');
+
+                    tempe_flash = data[1];
+                    humid_flash = data[2];
+
+                    string tempeflash_1 = tempe_flash + '\u2103' + "\r\n";
+                    string humidflash_1 = humid_flash + "%RH"+ "\r\n";
+                    txt_tempeflash.Invoke(new Action(() => txt_tempeflash.AppendText(tempeflash_1)));
+                    txt_humidflash.Invoke(new Action(() => txt_humidflash.AppendText(humidflash_1)));
+                }
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+           //var set = Convert.ToDouble(set_point);
+           //Invoke(new MethodInvoker(() => draw1(set)));
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cBoxCom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            
+            string data_send = "1";
+            serialPort1.Write(data_send);
+          
+        }
+
+
+    }
+}
